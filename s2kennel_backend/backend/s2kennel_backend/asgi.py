e1fc9308e3bf3da1f84c1cea3550f133b8cf49ff
neeraj sharma <2310074@ritindia.edu>
2026-07-16 14:53:33 +0530

    Fix Django settings module path for deployment

--- s2kennel_backend/backend/s2kennel_backend/asgi.py
+++ s2kennel_backend/backend/s2kennel_backend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 's2kennel_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.s2kennel_backend.settings')
 
 application = get_asgi_application()
