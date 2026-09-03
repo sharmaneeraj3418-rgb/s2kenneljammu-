8d96e8dbd05fbcb7e01ed9dbad4d02b58756898a
neeraj sharma <2310074@ritindia.edu>
2026-09-04 00:32:45 +0530

    Fix wsgi settings module path

--- s2kennel_backend/backend/s2kennel_backend/wsgi.py
+++ s2kennel_backend/backend/s2kennel_backend/wsgi.py
@@ -8,6 +8,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+if os.environ.get('DJANGO_SETTINGS_MODULE') is None:
+    if os.path.exists('backend/s2kennel_backend/settings.py'):
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.s2kennel_backend.settings'
+    else:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 's2kennel_backend.settings'
 
 application = get_wsgi_application()
